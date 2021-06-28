a9f462e40845b4a7fb0184c24d22303c7812d0dd
DS <deborah@havenware.com>
2021-06-28 09:08:15 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+
+loop = 1
+while (loop < 3):
+ 
+    color = input ("Enter a color: ")
+    plural_noun = input ("Enter a another color: ")
+    person = input ("Enter a person: ")
+    adjective = input ("Enter something you like about that person: ")
+
+
+    print("Roses are " + color)
+    print(plural_noun + " are blue")
+    print("I love " + person)
+    print(person + " " + adjective)
+
+    loop = loop + 1
